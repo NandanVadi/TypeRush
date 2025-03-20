54c9f2406f57b74bd417ea1538a205287edc24d6
NandanVadi <nandanvadi@gmail.com>
2025-03-20 13:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startBtn = document.querySelector(".start-btn");
+
+    startBtn.addEventListener("click", () => {
+        alert("Game Starting Soon!");
+    });
+});
